07f2b0a272a9de5fbd7a9e182f481eeb4e4938b7
Adam Davis <adam.davis@greenlight.guru>
2017-08-15 15:26:01 -0400

    Added standard express dependencies

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // resolved path to the base of the compiled client
 const publicPath = path.resolve(`${__dirname}/../public`);
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
 app.use(express.static(publicPath));
 
 // make sure this route is the last one in the list, or else API calls will not work
